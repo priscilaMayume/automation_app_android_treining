78b4899f67cfa4b76ab0d6cb70ce46d58b5a13bc
priscila hirotsu <priscila.hirotsu@conductor.com.br>
2021-03-19 13:41:34 -0300

    automação inicial da calculadora - soma

---
+++ src/main/java/br/com/pmshirotsu/appium/CalculadoraTeste.java
@@ -0,0 +1,38 @@
+package br.com.pmshirotsu.appium;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class CalculadoraTeste {
+    public static void main(String[] args) throws MalformedURLException {
+
+        //capacidades
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("deviceName", "emulator-5554");
+        desiredCapabilities.setCapability("automationName", "uiautomator2");
+        desiredCapabilities.setCapability("appPackage", "com.android.calculator2");
+        desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+
+        //driver de conexao e inicializar
+        AndroidDriver<WebElement> driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+
+        //Fazendo uma busca por elemente - por id
+        MobileElement el4 = (MobileElement) driver.findElementById("com.android.calculator2:id/digit_2");
+        el4.click();
+        MobileElement el5 = (MobileElement) driver.findElementByAccessibilityId("plus");
+        el5.click();
+        MobileElement el6 = (MobileElement) driver.findElementById("com.android.calculator2:id/digit_2");
+        el6.click();
+        MobileElement el7 = (MobileElement) driver.findElementById("com.android.calculator2:id/result");
+        el7.click();
+
+        //encerar a sessão
+        driver.quit();
+    }
+}
